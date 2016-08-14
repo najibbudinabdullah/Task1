26a4bf8c35a49dc86294fcb3d4275dc8b3599ade
najibbudinabdullah <najibbudinabdullah@gmail.com>
2016-08-14 12:49:04 +0700

    4 commit

--- app/src/main/java/com/najib/task1/MainActivity.java
+++ app/src/main/java/com/najib/task1/MainActivity.java
@@ -9,9 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import static android.view.View.OnClickListener;
 import static android.view.View.OnTouchListener;
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Button button0,button1,button2,button3,button4,button5,button6,button7,button8,button9,button10,button12;
     EditText edt1,edt2;
     String evalue ="1";
@@ -24,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edt1 = (EditText) findViewById(R.id.editText);
+        edt2 = (EditText) findViewById(R.id.editText2);
         button0 = (Button) findViewById(R.id.button0);
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
@@ -36,11 +37,21 @@ public class MainActivity extends AppCompatActivity {
         button9 = (Button) findViewById(R.id.button9);
         button10 = (Button) findViewById(R.id.button10);
         button12 = (Button) findViewById(R.id.button12);
-        fab = (FloatingActionButton) findViewById(R.id.fab);
         result = (TextView) findViewById(R.id.hasil);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
 
-        edt1 = (EditText) findViewById(R.id.editText);
-        edt2 = (EditText) findViewById(R.id.editText2);
+        button0.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        button10.setOnClickListener(this);
+        button12.setOnClickListener(this);
 
         edt1.setOnTouchListener(new OnTouchListener()
         {
@@ -63,170 +74,63 @@ public class MainActivity extends AppCompatActivity {
        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                num1 = Integer.parseInt(edt1.getText().toString());
-                num2 = Integer.parseInt(edt2.getText().toString());
 
-                hasil = (num1 - num2);
-                result.setText(hasil.toString());
-
-            }
-        });
-
-        button1.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(evalue=="1") {
-                    int start =edt1.getSelectionStart();
-                    edt1.setText(edt1.getText().insert(start,"1"));
-                    edt1.setSelection(start+1);
+                if(edt1.length() <= 0 || edt2.length() <= 0) {
+                    result.setText("0");
                 }else{
-                    int start2 =edt2.getSelectionStart();
-                    edt2.setText(edt2.getText().insert(start2,"1"));
-                    edt2.setSelection(start2+1);
+                    num1 = Integer.parseInt(edt1.getText().toString());
+                    num2 = Integer.parseInt(edt2.getText().toString());
+                    hasil = num1 - num2;
+                    result.setText(hasil.toString());
                 }
             }
         });
+    }
 
-        button2.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(evalue=="1") {
-                    int start =edt1.getSelectionStart();
-                    edt1.setText(edt1.getText().insert(start,"2"));
-                    edt1.setSelection(start+1);
-                }else{
-                    int start2 =edt2.getSelectionStart();
-                    edt2.setText(edt2.getText().insert(start2,"2"));
-                    edt2.setSelection(start2+1);
-                }
-            }
-        });
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button0:
+                tombol_angka(evalue,"0");
+                break;
 
-        button3.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(evalue=="1") {
-                    int start =edt1.getSelectionStart();
-                    edt1.setText(edt1.getText().insert(start,"3"));
-                    edt1.setSelection(start+1);
-                }else{
-                    int start2 =edt2.getSelectionStart();
-                    edt2.setText(edt2.getText().insert(start2,"3"));
-                    edt2.setSelection(start2+1);
-                }
-            }
-        });
+            case R.id.button1:
+                tombol_angka(evalue,"1");
+                break;
 
-        button4.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(evalue=="1") {
-                    int start =edt1.getSelectionStart();
-                    edt1.setText(edt1.getText().insert(start,"4"));
-                    edt1.setSelection(start+1);
-                }else{
-                    int start2 =edt2.getSelectionStart();
-                    edt2.setText(edt2.getText().insert(start2,"4"));
-                    edt2.setSelection(start2+1);
-                }
-            }
-        });
+            case R.id.button2:
+                tombol_angka(evalue,"2");
+                break;
 
-        button5.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(evalue=="1") {
-                    int start =edt1.getSelectionStart();
-                    edt1.setText(edt1.getText().insert(start,"5"));
-                    edt1.setSelection(start+1);
-                }else{
-                    int start2 =edt2.getSelectionStart();
-                    edt2.setText(edt2.getText().insert(start2,"5"));
-                    edt2.setSelection(start2+1);
-                }
-            }
-        });
+            case R.id.button3:
+                tombol_angka(evalue,"3");
+                break;
 
-        button6.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(evalue=="1") {
-                    int start =edt1.getSelectionStart();
-                    edt1.setText(edt1.getText().insert(start,"6"));
-                    edt1.setSelection(start+1);
-                }else{
-                    int start2 =edt2.getSelectionStart();
-                    edt2.setText(edt2.getText().insert(start2,"6"));
-                    edt2.setSelection(start2+1);
-                }
-            }
-        });
+            case R.id.button4:
+                tombol_angka(evalue,"4");
+                break;
 
-        button7.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(evalue=="1") {
-                    int start =edt1.getSelectionStart();
-                    edt1.setText(edt1.getText().insert(start,"7"));
-                    edt1.setSelection(start+1);
-                }else{
-                    int start2 =edt2.getSelectionStart();
-                    edt2.setText(edt2.getText().insert(start2,"7"));
-                    edt2.setSelection(start2+1);
-                }
-            }
-        });
+            case R.id.button5:
+                tombol_angka(evalue,"5");
+                break;
 
-        button8.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(evalue=="1") {
-                    int start =edt1.getSelectionStart();
-                    edt1.setText(edt1.getText().insert(start,"8"));
-                    edt1.setSelection(start+1);
-                }else{
-                    int start2 =edt2.getSelectionStart();
-                    edt2.setText(edt2.getText().insert(start2,"8"));
-                    edt2.setSelection(start2+1);
-                }
-            }
-        });
+            case R.id.button6:
+                tombol_angka(evalue,"6");
+                break;
 
-        button9.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(evalue=="1") {
-                    int start =edt1.getSelectionStart();
-                    edt1.setText(edt1.getText().insert(start,"9"));
-                    edt1.setSelection(start+1);
-                }else{
-                    int start2 =edt2.getSelectionStart();
-                    edt2.setText(edt2.getText().insert(start2,"9"));
-                    edt2.setSelection(start2+1);
-                }
-            }
-        });
+            case R.id.button7:
+                tombol_angka(evalue,"7");
+                break;
 
-        button0.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(evalue=="1") {
-                    int start =edt1.getSelectionStart();
-                    edt1.setText(edt1.getText().insert(start,"0"));
-                    edt1.setSelection(start+1);
-                }else{
-                    int start2 =edt2.getSelectionStart();
-                    edt2.setText(edt2.getText().insert(start2,"0"));
-                    edt2.setSelection(start2+1);
-                }
-            }
-        });
+            case R.id.button8:
+                tombol_angka(evalue,"8");
+                break;
 
-        button10.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            case R.id.button9:
+                tombol_angka(evalue,"9");
+                break;
 
-                if(evalue=="1") {
+            case R.id.button10:
+                if(evalue=="1"){
                     String dialled_nos = edt1.getText().toString();
                     int remove_index_position = edt1.getSelectionStart()-1;
                     StringBuilder dialled_nos_builder = new StringBuilder(dialled_nos);
@@ -235,8 +139,6 @@ public class MainActivity extends AppCompatActivity {
                         edt1.setText(dialled_nos_builder.toString());
                         edt1.setSelection(remove_index_position);
                     }
-
-
                 }else{
                     String dialled_nos = edt2.getText().toString();
                     int remove_index_position = edt2.getSelectionStart()-1;
@@ -247,18 +149,30 @@ public class MainActivity extends AppCompatActivity {
                         edt2.setSelection(remove_index_position);
                     }
                 }
-            }
-        });
+                break;
 
-        button12.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            case R.id.button12:
                 if(evalue=="1") {
                     edt1.setText("");
                 }else{
                     edt2.setText("");
                 }
-            }
-        });
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public void tombol_angka(String value, String angka){
+        if(value=="1") {
+            int start =edt1.getSelectionStart();
+            edt1.setText(edt1.getText().insert(start,angka));
+            edt1.setSelection(start+1);
+        }else{
+            int start2 =edt2.getSelectionStart();
+            edt2.setText(edt2.getText().insert(start2,angka));
+            edt2.setSelection(start2+1);
+        }
     }
 }
